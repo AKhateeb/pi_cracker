aa5cc8ce890ea43d58cc11247f9f5a170f267d57
abdulkader <abdulkader@ss-solutions.net>
2018-12-05 14:12:54 +0200

    some edits

--- pi_cracker/settings.py
+++ pi_cracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&@sxb8r5z9lch9n5cycw2u@y5@0=_n6twn1nt4kt2-muq^i4u2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.2', '127.0.0.1', 'raspberrypi']
+ALLOWED_HOSTS = ['192.168.0.2', '127.0.0.1', 'raspberrypi','172.11.11.28']
 
 
 # Application definition
