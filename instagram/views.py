adaa9269b8aaffd8cd656b16652ac9d9903da840
abdulkader <abdulkader@ss-solutions.net>
2018-12-06 15:33:23 +0200

    set passwprd generator in notebook

--- instagram/views.py
+++ instagram/views.py
@@ -1,15 +1,26 @@
 import re
 from django.shortcuts import render
-import instagram
+from subprocess import Popen, PIPE, STDOUT
 
 cracker = None
-current_password = "xxxx"
-
+output = None
+exitstatus = None
 
 def index(request):
-    return render(request, template_name=__package__+".html",context=None)
+	return render(request, template_name=__package__+".html",context=None)
 
 def start(request):
-    username = request.GET.get('username', None)
-    if username and re.match(r"^[a-zA-Z0-9._]+$", username):
-        cracker = ""
+	username = request.GET.get('username', None)
+	pass_list = ['ffff','dftghfd','frtgyhujy']
+	command = ["python","instagram","-u", username, "-f", pass_list, "16"]
+	if username and re.match(r"^[a-zA-Z0-9._]+$", username):
+		cracker = Popen(command, stdout=PIPE, stderr=STDOUT)
+		output = cracker.stdout.read()
+		exitstatus = cracker.poll()
+
+def check_status(request):
+	result = {"output":str(output), "status": ""}
+	if exitstatus==0:
+		result["status"] = "Success"
+	else:
+		result["status"] = "Failed"
